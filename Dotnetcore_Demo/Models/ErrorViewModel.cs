964cb613586a8fd782f6d447755afda06303b806
Gokila <Gokila@BTS-IN-DEV1>
2023-02-14 15:23:12 +0530

    Add project files.

---
+++ Dotnetcore_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dotnetcore_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
